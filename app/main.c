c01b781a867f0de0d947abf96dd52c26a8d2d00f
smit <smit@simform.com>
2026-02-03 15:16:24 +0530

    feat: add comments for three blocks after stashing half of them once

--- app/main.c
+++ app/main.c
@@ -4,11 +4,16 @@
 
 int main () {
 
+    // comment for i
     int i = 12;
 
+    // comment for conditinal
     if (i == 12){
         printf("i is twelve");
     }
 
+    // comment for return statement.
+
+    // amended comments.
     return 0;
 }
